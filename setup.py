54d0390c366b20bb190b311baabc2b446ac25721
Krishn899 <radhaykrishna699@gamil.com>
2025-11-26 14:35:35 +0530

    made a setup.py for the project

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+##this function takes file path as an argument and returns a list as an output
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[requirement.replace('\n','')for requirement in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+
+    return requirements
+
+setup(
+    name='ml project',
+    version='1.0.0',
+    author='Radhay krishna',
+    author_email='radhaykrishna699@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
